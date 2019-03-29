00af55a85c32fd5346d5b85633090581cbbb3d52
Jack Nelson <jackhnelson0@gmail.com>
2019-03-29 02:11:36 -0400

    Delete index.js

--- Sampling/Sampling/js/index.js
+++
@@ -1,9 +0,0 @@
-// jQuery function utilizing a button to hide and display new content windows.
-
-$(document).ready(function(){
-    $(".content-button").click(function(){
-        $(".content-container").fadeOut(2000);
-        $(".illusion-container, .spinner-masthead").delay(2000).fadeIn(2000);
-        $(".illusion-container").css("display", "flex");
-    });
-});
