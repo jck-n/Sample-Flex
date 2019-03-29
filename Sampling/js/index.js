028e08635da6b4ad8d92db97bb6abcfb6f283bcc
Jack Nelson <jackhnelson0@gmail.com>
2019-03-29 02:11:20 -0400

    finished new illusion, needs touch-up

--- Sampling/js/index.js
+++ Sampling/js/index.js
@@ -3,6 +3,7 @@
 $(document).ready(function(){
     $(".content-button").click(function(){
         $(".content-container").fadeOut(2000);
-        $(".illusion-container").delay(2000).fadeIn(2000);
+        $(".illusion-container, .spinner-masthead").delay(2000).fadeIn(2000);
+        $(".illusion-container").css("display", "flex");
     });
 });
