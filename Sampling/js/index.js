f071b559ddb891db749cf42c946383a67b1faf0c
Jack Nelson <jackhnelson0@gmail.com>
2019-03-29 02:30:27 -0400

    added touch-ups to new illusion

--- Sampling/js/index.js
+++ Sampling/js/index.js
@@ -1,9 +1,9 @@
 // jQuery function utilizing a button to hide and display new content windows.
 
 $(document).ready(function(){
+    $(".illusion-container").fadeOut();
     $(".content-button").click(function(){
-        $(".content-container").fadeOut(2000);
+        $(".content-container, .content-button").fadeOut(2000);
         $(".illusion-container, .spinner-masthead").delay(2000).fadeIn(2000);
-        $(".illusion-container").css("display", "flex");
     });
 });
