90fcffce38423beaae40d60fc5832178919dbc20
jck-n <jackhnelson0@gmail.com>
2019-03-30 01:23:10 -0400

    minor updates, testing git terminal

--- Sampling/js/index.js
+++ Sampling/js/index.js
@@ -1,7 +1,7 @@
 // jQuery function utilizing a button to hide and display new content windows.
 
 $(document).ready(function(){
-    $(".illusion-container").fadeOut();
+    $(".illusion-container").hide();
     $(".content-button").click(function(){
         $(".content-container, .content-button").fadeOut(2000);
         $(".illusion-container, .spinner-masthead").delay(2000).fadeIn(2000);
