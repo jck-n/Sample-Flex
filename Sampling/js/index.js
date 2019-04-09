cc00750571309089dc28af6cbb4999b4e746665c
jck-n <jackhnelson0@gmail.com>
2019-04-09 00:43:11 -0400

    tidied up js file, plan on swapping to pure JS/LESS

--- Sampling/js/index.js
+++ Sampling/js/index.js
@@ -6,14 +6,9 @@ $(document).ready(function(){
         $(".content-container, .content-button").fadeOut(2000);
         $(".illusion-container, .spinner-masthead").delay(2000).fadeIn(2000);
     });
-});
-$(document).ready(function(){
     $(".spinner-button").click(function(){
         $(".illusion-square").fadeOut(2000);
         $(this).text("Click to Re-Enable Cover Boxes");
         $("button:last-child").addClass("recover");
-    });
-    $(".recover").click(function(){
-        $(".illusion-square").fadeIn(2000);
     });
 });
